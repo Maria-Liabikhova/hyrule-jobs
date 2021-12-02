d97b494c666ecfee8177f2655a9157a9037caf5e
Мария Лябихова <mashagi@yandex.ru>
2021-12-02 19:30:42 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,40 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="app">hello, hydrule
+    <p>{{jobs}}</p>
   </div>
-  <router-view/>
 </template>
+
+<script lang="ts">
+// import { defineComponent, reactive, toRefs } from 'vue'
+import { defineComponent, ref } from 'vue';
+import Job from "@/types/Job"
+
+export default defineComponent({
+  name: "App",
+  setup(){
+    // const state = reactive({
+    //   name: "link" as string,
+    //   age: 25 as number | string
+    // })
+    // return {...toRefs(state)}
+
+    const jobs=ref<Job[]>( [
+      {title: "frontend developer", location: "Voronez", salary: 150000, takesRelocation: false, id: 1},
+      {title: "Pediatrician", location: "St-Petersburg", salary: 80000, takesRelocation: true, id: 2},
+      {title: "Teacher", location: "Moscow", salary: 60000, takesRelocation: true, id: 3},
+      {title: "Flute player", location: "St-Petersburg", salary: 75000, takesRelocation: true, id: 4},
+      {title: "Farm worker", location: "Rostov", salary: 50000, takesRelocation: true, id:5},
+    ])
+    return {jobs}
+  }
+})
+</script>
+
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
---
+++ src/types/Job.ts
@@ -0,0 +1,8 @@
+interface Job {
+  title: string,
+  location: string,
+  salary: number,
+  takesRelocation: boolean,
+  id: number
+}
+export default Job
